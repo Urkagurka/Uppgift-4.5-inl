dd49d3c2c5414027576ed4be20c9233ec85dd332
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-21 16:50:14 +0200

    Add project files.

---
+++ Uppgift 4.5 inl/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Uppgift_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string input = Console.ReadLine();
+            string output = "";
+
+            for (int i = 0;i< input.Length;i++)
+            {
+                string tecken = input[i].ToString ();
+                try
+                {
+                    int siffra = int.Parse(tecken) + 1;
+                    //    ++siffra;
+                    //    siffra = siffra +  1;
+                    if (siffra == 10)
+                    {
+                        siffra = 0;
+                    }
+                    output = output + siffra;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine ("tecken är inte en siffra: " + tecken);
+                }
+            }
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
